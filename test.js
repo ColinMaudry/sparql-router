6ff803d26c272e0e1cb7f83170c1ef49962799c7
Colin Maudry <colin@maudry.com>
2015-11-29 00:32:02 +0100

    Working setup with tests

---
+++ test.js
@@ -0,0 +1,15 @@
+var request = require('supertest');
+var app = require('./app');
+
+describe('Test requests to the root path', function() {
+
+	it('Returns a 200 status code', function(done) {
+		request(app)
+			.get('/') 
+			.expect(200)
+			.end(function (error) {
+			if (error) throw error;
+			done();
+	});
+	});
+}); 
