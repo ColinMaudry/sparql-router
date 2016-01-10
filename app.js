fa22fd3e0e40c31de80a4b3519ab724a5571d746
Colin Maudry <colin@maudry.com>
2016-01-10 16:46:48 +0100

    App runs and display public/index.html on root path

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/',function(request,response) {
-	response.send('OK');
+	response.send("Hello");
+});
+app.get('/bla',function(request,response) {
+	response.send("Hello");
 });
 
 module.exports = app;
+
+app.listen(3000,function(){
+	console.log('Started app on port 3000.')
+});
