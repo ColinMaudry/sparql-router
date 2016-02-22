eb63955cecb626a2eae626099fd028e94eb5aa59
Colin Maudry <colin@maudry.com>
2016-02-22 19:19:04 +0100

    Added config template

---
+++ config_template.js
@@ -0,0 +1,9 @@
+var config = {
+	endpoint : {
+		host: "localhost",
+		port: 3030,
+		queryPath: '/ds/query',
+		queryParameterName: 'query'
+	},
+	defaultAccept : "application/json+sparql-results"
+}
