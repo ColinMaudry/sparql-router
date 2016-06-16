95deb4f52263acdabbffbaa662ed130f4685119e
Colin Maudry <colin@maudry.com>
2016-06-16 18:10:50 +0200

    Enabled basic routing #61

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
 import Boostrap from './vendor/bootstrap.js'
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import NewQuery from './components/NewQuery.vue'
+Vue.use(VueRouter);
+
+
 
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+// new Vue({
+//   el: 'body',
+//   components: { App },
+//   routes : {
+//     '/new' : {
+//       componentId: "App",
+//       isDefault: true
+//     }
+//   }
+// })
+
+var App = Vue.extend({});
+var router = new VueRouter()
+
+router.map({
+    '/': {
+        component: NewQuery,
+        name : "new"
+    }
 })
+
+router.start(App, '#app');
