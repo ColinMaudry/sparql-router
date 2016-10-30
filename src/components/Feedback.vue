12e26c15be065baa4b56895624597815c4c70a4d
Colin Maudry <colin@maudry.com>
2016-10-30 18:17:43 +0100

    Added my twitter handle #54

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -3,7 +3,7 @@
     <h3 id="email">Email</h3>
       <p><a href="mailto:colin@maudry.com?subject=[{{ ip }}@queery.link]">colin@maudry.com</a></p>
       <h3 id="twitter-chat">Twitter chat</h3>
-      <p>If you have a Twitter account, you can <a href="https://twitter.com/CMaudry">chat with me</a> even if you don't follow me.</p>
+      <p>If you have a Twitter account, you can <a href="https://twitter.com/CMaudry">chat with me (cmaudry)</a> even if you don't follow me.</p>
       <p>You can also tweet to <a href="http://twitter.com/QueeryLink">@QueeryLink</a> or with #QueeryLink tag.</p>
       <h3 id="-help-">"Help!"</h3>
       <p>If you contact me about trouble using queery.link, please give me your IP address: <span class="ip text-primary">{{ ip }}</span></p>
