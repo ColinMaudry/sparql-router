c136628277f2b46635f43c7e491e0db7a484ff92
Colin Maudry <colin@maudry.com>
2016-10-30 22:03:32 +0100

    Improved feedback page and added one more example query #54

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
     <h3 id="email">Email</h3>
-      <p><a href="mailto:colin@maudry.com?subject=[{{ ip }}@queery.link]">colin@maudry.com</a></p>
-      <h3 id="twitter-chat">Twitter chat</h3>
-      <p>If you have a Twitter account, you can <a href="https://twitter.com/CMaudry">chat with me (cmaudry)</a> even if you don't follow me.</p>
-      <p>You can also tweet to <a href="http://twitter.com/QueeryLink">@QueeryLink</a> or with #QueeryLink tag.</p>
-      <h3 id="-help-">"Help!"</h3>
-      <p>If you contact me about trouble using queery.link, please give me your IP address: <span class="ip text-primary">{{ ip }}</span></p>
-      <p>This way I can easily filter your activity in the server logs ;-)</p>
-      <h3>Comments (refresh to see them)</h3>
-      <div id="disqus_thread"></div>
+    <p><a href="mailto:colin@maudry.com?subject=[{{ ip }}@queery.link]">colin@maudry.com</a></p>
+    <h3 id="twitter-chat">Twitter chat</h3>
+    <p>If you have a Twitter account, you can <a href="https://twitter.com/CMaudry">chat with me (cmaudry)</a> even if you don't follow me.</p>
+    <p>You can also tweet to <a href="http://twitter.com/QueeryLink">@QueeryLink</a> or with #QueeryLink tag.</p>
+    <h3>Github</h3>
+    <p>The issue tracket is waiting for your bug reports and feature requests!</p>
+    <p>Make sure you first check the <a href="https://github.com/ColinMaudry/sparql-router/issues?q=is%3Aissue+is%3Aopen+-label%3Abug" target="_blank">upcoming features</a> and the <a href="https://github.com/ColinMaudry/sparql-router/issues?q=is%3Aissue+is%3Aopen+label%3Abug" target="_blank">known issues</a>.</p>
+    <h3 id="-help-">"Help!"</h3>
+    <p>If you contact me about trouble using queery.link, please give me your IP address: <span class="ip text-primary">{{ ip }}</span></p>
+    <p>This way I can easily filter your activity in the server logs ;-)</p>
     <h3>Drop a comment</h3>
     <div id="disqus_thread"></div>
   </div>
